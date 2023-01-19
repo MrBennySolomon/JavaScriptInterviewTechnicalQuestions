c69f264b1a09e84b304d6a2218617949b7e4b2f5
bensol <mr.benny.solomon@gmail.com>
2023-01-19 17:19:55 +0200

    Ex4.1

---
+++ Ex4.1 - Fibonacci/script.js
@@ -0,0 +1,17 @@
+
+const fibonacci = n => {
+  let result = [0, 1];
+  let temp = 0;
+  let n1 = 1;
+  let n2 = 0;
+
+  while (n1 < n) {
+    result.push(n1 + n2);
+
+    temp = n1;
+    n1 = n1 + n2;
+    n2 = temp;
+    
+  }
+  return result;
+}
