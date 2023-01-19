98c083b22cc274c5a4a2552722129adccf4febcd
MRBENNYSOLOMON <mr.benny.solomon@gmail.com>
2023-01-19 20:20:58 +0200

    Ex5.3

--- Ex5.3 - To Camel Case/script.js
+++ Ex5.3 - To Camel Case/script.js
@@ -1,14 +1,12 @@
-'use strict';
-
 const toCamelCase = str => {
-
+  let arr = [];
   const newWord = [];
   if (str.includes('-')) {
-    const arr = str.split('-');
+    arr = str.split('-');
     newWord.push(arr[0]);
   }else if (str.includes('_')) {
-    const arr = str.split('_');
-    newWord.push(arr[0].charAt(0).toUpperCase() + arr[i].slice(1));
+    arr = str.split('_');
+    newWord.push(arr[0].charAt(0).toUpperCase() + arr[0].slice(1));
   }else {
     throw new Error('string contains invalid delimiter character');
   }
@@ -22,4 +20,4 @@ const toCamelCase = str => {
 
 
 console.log(toCamelCase("the-stealth-warrior"));
-//console.log(toCamelCase('The_Stealth_Warrior'));
+console.log(toCamelCase('The_Stealth_Warrior'));
