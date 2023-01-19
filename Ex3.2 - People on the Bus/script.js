035f61a943ff09c4f54e352c4220de1af811178c
bensol <mr.benny.solomon@gmail.com>
2023-01-19 16:44:00 +0200

    Ex3.2

---
+++ Ex3.2 - People on the Bus/script.js
@@ -0,0 +1,65 @@
+
+
+
+
+
+
+
+
+
+
+
+
+const data = 
+[
+{
+  busStop: "start",
+  inAndOut:[2, 0]
+},
+{
+  busStop: "one",
+  inAndOut:[2, 1]
+},
+{
+  busStop: "two",
+  inAndOut:[2, 1]
+},
+{
+  busStop: "three",
+  inAndOut:[2, 1]
+},
+{
+  busStop: "four",
+  inAndOut:[2, 1]
+},
+{
+  busStop: "five",
+  inAndOut:[2, 1]
+},
+{
+  busStop: "final",
+  inAndOut:[0, 1]
+}
+];
+
+const checkPositive = (onTheBus, busStop) => {
+
+  if (onTheBus - busStop >= 0) {
+    return busStop;
+  }else {
+    throw new Error('The bus is empty');
+  }
+}
+
+const busRoute = data => {
+
+  let onTheBus = 0;
+  for (const busStop of data) {
+      
+    onTheBus += busStop['inAndOut'][0];
+    onTheBus -= checkPositive(onTheBus, busStop['inAndOut'][1]);
+      
+  }
+  
+  return onTheBus;
+}
