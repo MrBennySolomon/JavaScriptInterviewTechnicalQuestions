f63bc88cbea0edf7b5eaa5c43171fa19fa6f66c5
bensol <mr.benny.solomon@gmail.com>
2023-01-19 15:08:02 +0200

    Ex2.7

---
+++ Ex2.7 - Basic Math/script.js
@@ -0,0 +1,12 @@
+const basicOp = (operation, value1, value2) => {
+  switch (operation) {
+    case '+': 
+      return value1 + value2;
+    case '-': 
+      return value1 - value2;
+    case '*': 
+      return value1 * value2;
+    case '/':
+      return value1 / value2;
+  }
+}
