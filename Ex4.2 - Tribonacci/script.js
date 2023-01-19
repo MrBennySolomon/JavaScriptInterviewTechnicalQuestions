bb1750b0e0e4fe9dceb90e9ce4d67dfdd4057e24
bensol <mr.benny.solomon@gmail.com>
2023-01-19 17:31:31 +0200

    Ex4.2

---
+++ Ex4.2 - Tribonacci/script.js
@@ -0,0 +1,21 @@
+const tribonacci = n => {
+  let result = [1, 1, 1];
+  let temp = 0;
+  let n1 = 1;
+  let n2 = 1;
+  let n3 = 1;
+
+
+  while (n1 <= n) {
+    result.push(n1 + n2 + n3);
+
+    temp = n1;
+    n1 = n1 + n2 + n3;
+    n3 = n2;
+    n2 = temp;
+    
+  }
+  return result;
+}
+
+console.log(tribonacci(144));
