fb96e66f5bfdcddfd201dc2efbd0d78342612f67
unknown <mr.benny.solomon@gmail.com>
2023-01-20 11:14:31 +0200

    Ex5.3 refactor

--- Ex5.3 - To Camel Case/script.js
+++ Ex5.3 - To Camel Case/script.js
@@ -1,15 +1,16 @@
+'use strict'
 const toCamelCase = str => {
   let arr = [];
   const newWord = [];
   if (str.includes('-')) {
     arr = str.split('-');
-    newWord.push(arr[0]);
   }else if (str.includes('_')) {
     arr = str.split('_');
-    newWord.push(arr[0].charAt(0).toUpperCase() + arr[0].slice(1));
   }else {
     throw new Error('string contains invalid delimiter character');
   }
+
+  newWord.push(arr[0]);
 
   for (let i = 1; i < arr.length; i++) {
     newWord.push(arr[i].charAt(0).toUpperCase() + arr[i].slice(1));
