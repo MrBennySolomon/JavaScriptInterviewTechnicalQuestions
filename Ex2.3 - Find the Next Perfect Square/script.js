3cd5d2575d83bc597d95edccfcd89ef8ae7c079c
unknown <mr.benny.solomon@gmail.com>
2023-01-20 03:30:33 +0200

    Ex2.3 fix

--- Ex2.3 - Find the Next Perfect Square/script.js
+++ Ex2.3 - Find the Next Perfect Square/script.js
@@ -1,16 +1,18 @@
+'use strict'
 const findNextSquare = n => {
   if (!Number.isInteger(Math.sqrt(n))) {
     return -1;
   }
 
   let next = n;
-  let flag = true;
   
-  while (flag) {
-    if (Math.sqrt(++next) % 2 === 0) {
+  while (true) {
+    if (Number.isInteger(Math.sqrt(++next))) {
       break;
     }
   }
 
   return next;
 }
+
+console.log(findNextSquare(114)); 
