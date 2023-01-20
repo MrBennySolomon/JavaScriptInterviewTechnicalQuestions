f5521becf246168e4ff34e857d662dd7a9f1cfef
unknown <mr.benny.solomon@gmail.com>
2023-01-20 10:41:13 +0200

    Ex5.8 refactor

--- Ex5.8 - shortest words version 2/script.js
+++ Ex5.8 - shortest words version 2/script.js
@@ -2,9 +2,8 @@ const longestWords = str => {
 
   const arr = str.split(' ');
 
-  arr.sort((a, b) => {
-    return a.length - b.length;
-  });
+  arr.sort((a, b) => a.length - b.length);
+
   const result = [];
   const long = arr[arr.length - 1].length;
   for (let i = arr.length - 1; i >= 0; i--) {
