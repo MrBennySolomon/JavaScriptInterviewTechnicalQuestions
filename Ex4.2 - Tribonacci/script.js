be712ffca495a6598efd584255a11199f41518f9
unknown <mr.benny.solomon@gmail.com>
2023-01-20 11:03:16 +0200

    Ex4.2 refactor

--- Ex4.2 - Tribonacci/script.js
+++ Ex4.2 - Tribonacci/script.js
@@ -1,10 +1,13 @@
-const tribonacci = n => {
-  let result = [1, 1, 1];
-  let temp = 0;
-  let n1 = 1;
-  let n2 = 1;
-  let n3 = 1;
+const tribonacci = (signatureArr, n) => {
+  if (n === 0) return [];
+  if (n === 1) return [signatureArr[0]];
+  if (n === 2) return [signatureArr[0], signatureArr[1]];
 
+  let result = signatureArr
+  let temp = 0;
+  let n1 = result[2];
+  let n2 = result[1];
+  let n3 = result[0];
 
   while (n1 <= n) {
     result.push(n1 + n2 + n3);
@@ -17,3 +20,5 @@ const tribonacci = n => {
   }
   return result;
 }
+
+console.log(tribonacci([1, 1, 1], 2));
