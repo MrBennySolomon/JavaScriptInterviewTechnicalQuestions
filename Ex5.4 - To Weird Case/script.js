98cd5b538ef19dbe205fa6f65b87231f1e9a77b0
unknown <mr.benny.solomon@gmail.com>
2023-01-20 11:19:15 +0200

    Ex5.4 refactor

--- Ex5.4 - To Weird Case/script.js
+++ Ex5.4 - To Weird Case/script.js
@@ -1,14 +1,13 @@
-//'use strict'
+'use strict'
 
 const toWeirdCase = str => {
   const arr = str.split('');
   for (let i = 0; i < str.length; i++) {
     if((i % 2) === 0) {
       arr.splice(i, 1, str[i].toUpperCase());
-      //str.replace(str.charAt(i), str.charAt(i).toUpperCase());
+    }else {
+      arr.splice(i, 1, str[i].toLowerCase());
     }
   }
   return arr.join('');
 }
-
-console.log(toWeirdCase('Weird string case'));
