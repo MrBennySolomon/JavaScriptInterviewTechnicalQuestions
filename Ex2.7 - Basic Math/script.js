322586b3051338cd9110d244a5daf31395cecd5d
unknown <mr.benny.solomon@gmail.com>
2023-01-20 02:42:39 +0200

    Ex2.7 refactor

--- Ex2.7 - Basic Math/script.js
+++ Ex2.7 - Basic Math/script.js
@@ -1,3 +1,4 @@
+'use strict'
 const basicOp = (operation, value1, value2) => {
   switch (operation) {
     case '+': 
@@ -8,5 +9,7 @@ const basicOp = (operation, value1, value2) => {
       return value1 * value2;
     case '/':
       return value1 / value2;
+    default:
+      throw new Error('The operator parameter is not correct...');
   }
 }
